735000dac28760f72fdcfcfc31535d8462ba3800
Jason Hatton <jason@hatton.io>
2023-10-06 00:53:38 -0500

    go fmt

--- nes/ines.go
+++ nes/ines.go
@@ -20,15 +20,15 @@ func NewCassette(path string) (Ines, error){
 	}
 
 	// validate header
-	if !(bytes[0] == 0x4e && bytes[1] == 0x45 && bytes[2] == 0x53 && bytes[3] == 0x1A){
+	if !(bytes[0] == 0x4e && bytes[1] == 0x45 && bytes[2] == 0x53 && bytes[3] == 0x1A) {
 		return nil, fmt.Errorf("validation magic error :this file is not nes file. [PATH] %s", path)
 	}
 
 	// prgRom = 0x4000 Byte (16KB) * header[4]
 	// chrRom = 0x2000 Byte (8KB) * header[5]
 	prgRomStart := HeaderSize
-	chrRomStart := HeaderSize + int(bytes[4]) * 0x4000
-	chrROMEnd := chrRomStart + int(bytes[5]) * 0x2000
+	chrRomStart := HeaderSize + int(bytes[4])*0x4000
+	chrROMEnd := chrRomStart + int(bytes[5])*0x2000
 
 	prgRom := bytes[prgRomStart:chrRomStart]
 	if bytes[4] == 1 {
@@ -36,26 +36,26 @@ func NewCassette(path string) (Ines, error){
 	}
 
 	return &Mapper0{
-		isHorizontalMirror:bytes[6] & 0x01 == 0,
-		prgRom:prgRom,
-		chrRom:bytes[chrRomStart:chrROMEnd],
+		isHorizontalMirror: bytes[6]&0x01 == 0,
+		prgRom:             prgRom,
+		chrRom:             bytes[chrRomStart:chrROMEnd],
 	}, nil
 }
 
-type Mapper0 struct{
-	prgRom []byte
-	chrRom []byte
+type Mapper0 struct {
+	prgRom             []byte
+	chrRom             []byte
 	isHorizontalMirror bool
 }
 
-func (m *Mapper0) PrgRom() []byte{
+func (m *Mapper0) PrgRom() []byte {
 	return m.prgRom
 }
 
-func (m *Mapper0) ChrRom() []byte{
+func (m *Mapper0) ChrRom() []byte {
 	return m.chrRom
 }
 
-func (m *Mapper0) IsHorizontalMirror() bool{
+func (m *Mapper0) IsHorizontalMirror() bool {
 	return m.isHorizontalMirror
 }
